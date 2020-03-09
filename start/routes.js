26c6c8ce945fc4a8a39d697c35509909193c3e90
gamonpo <gamonpo@outlook.com>
2020-03-09 14:43:24 -0300

    Identando

--- start/routes.js
+++ start/routes.js
@@ -1,86 +1,82 @@
 'use strict'
 const Route = use('Route')
 
-//-----------------------------------------------------------------------------//
+/// -----------------------------------------------------------------------------//
 
-//EmpresaUsuario
+// EmpresaUsuario
 
 Route.group(() => {
   Route.resource('/empresa_usuarios', 'EmpresaUsuarioController')
-  .apiOnly()  
-
+    .apiOnly()
 }).middleware(['auth', 'is:(farmaceutico)'])
 
-//-----------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------//
 
-//Users
+// Users
 
 Route.group(() => {
   Route.resource('/users', 'UserController')
-  .apiOnly()
-  .except(['store'])
+    .apiOnly()
+    .except(['store'])
 
   Route.post('/users', 'UserController.store').validator('User')
-
 }).middleware(['auth', 'is:(farmaceutico)'])
 
-//-----------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------//
 
-//Permissions
+// Permissions
 
 Route.resource('permissions', 'PermissionController')
   .apiOnly()
   .middleware(['auth', 'is:(farmaceutico)'])
 
-//-----------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------//
 
-//Roles
+// Roles
 
 Route.resource('roles', 'RoleController')
   .apiOnly()
   .middleware(['auth', 'is:(farmaceutico)'])
 
-//-----------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------//
 
-//Session
-Route.post('sessions', 'SessionController.store').validator('Session') 
+// Session
+Route.post('sessions', 'SessionController.store').validator('Session')
 
-//-----------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------//
 
-//Password
+// Password
 
 Route.post('passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
 
 Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword')
 
-//-----------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------//
 
-//Produtos
+// Produtos
 
 Route.group(() => {
   Route.resource('/produtos', 'ProdutoController')
-  .apiOnly()
-  .except(['store'])
+    .apiOnly()
+    .except(['store'])
 
   Route.post('produtos', 'ProdutoController.store').validator('Produto')
-
 }).middleware(['auth', 'is:(farmaceutico)'])
 
-//-----------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------//
 
-//Empresas
+// Empresas
 Route.group(() => {
   Route.resource('/empresas', 'EmpresaController')
-  .apiOnly()
-  .except(['store'])
+    .apiOnly()
+    .except(['store'])
 
   Route.post('empresas', 'EmpresaController.store').validator('Empresa')
-
 }).middleware(['auth', 'is:(farmaceutico)'])
 
-//-----------------------------------------------------------------------------//
+// -----------------------------------------------------------------------------//
 
-//Estoques
+// Estoques
 
 Route.get('/estoques', 'EstoqueController.index')
   .middleware(['auth', 'can:read_estoques'])
@@ -90,14 +86,10 @@ Route.get('/estoques/:id', 'EstoqueController.show')
 
 Route.group(() => {
   Route.resource('/estoques', 'EstoqueController')
-  .apiOnly()
-  .except(['index', 'show', 'store'])
-  
-  Route.post('estoques', 'EstoqueController.store').validator('Estoque')
+    .apiOnly()
+    .except(['index', 'show', 'store'])
 
+  Route.post('estoques', 'EstoqueController.store').validator('Estoque')
 }).middleware(['auth', 'is:(farmaceutico)'])
 
 // password_confirmation -> reset da senha, cria user
-
-
-
